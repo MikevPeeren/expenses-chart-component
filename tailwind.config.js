d63ddaf8928aaf8f4983be907c22680e9a59f8c3
MikevPeeren <mikevpeeren@hotmail.com>
2022-06-16 21:46:42 +0200

    feature: added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "hsl(10, 79%, 65%)",
+        cyan: "hsl(186, 34%, 60%)",
+        darkBrown: "hsl(25, 47%, 15%)",
+        mediumBrown: "hsl(28, 10%, 53%)",
+        cream: "hsl(27, 66%, 92%)",
+        veryPaleOrange: "hsl(33, 100%, 98%)",
+      },
+    },
   },
   variants: {
     extend: {},
